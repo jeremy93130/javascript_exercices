92755c3274ac79b9ce84c9ded0754e6344b5addd
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-05-23 13:35:33 +0200

    exercices commencement js

---
+++ exercice/js/exercice.js
@@ -0,0 +1,18 @@
+var prenom = "Jeremy"; // string
+
+var myNumber = 65; // number / nombre
+
+var monNum = "100"; // string
+
+// var nom = izadi; // synthaxe causant une erreur
+
+var booleanFalse = false; //boolean
+var booleanTrue = true; // boolean
+
+var varNull = null; //null
+
+var array = [];
+var object = {};
+
+console.log(myNumber + monNum);
+console.log(prenom);
