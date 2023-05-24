5c44885803b7293cd00f50d594701769e6f27e0c
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-05-24 15:15:42 +0200

    avancement exercices js

--- exercice/js/exercice.js
+++ exercice/js/exercice.js
@@ -1,6 +1,6 @@
 var prenom = "Jeremy"; // string
 
-var myNumber = 65; // number / nombre
+var myNumber = 31; // number / nombre
 
 var monNum = "100"; // string
 
@@ -14,5 +14,53 @@ var varNull = null; //null
 var array = [];
 var object = {};
 
-console.log(myNumber + monNum);
-console.log(prenom);
+// // console.log(myNumber + monNum);
+// // console.log(prenom);
+// // alert(`je suis ${prenom} et j'ai ${myNumber} ans`);
+// // window.alert(`je suis ${prenom} et j'ai ${myNumber} ans`);
+// console.log(booleanFalse);
+// console.log(booleanTrue);
+// // window.alert(prenom);
+
+// // document.write("à la pause vous aurez des Haribos Pik !");
+
+// // window.prompt("question: quel âge as-tu?", "âge");
+// // var age = prompt("question: quel âge as-tu?", "âge");
+// // console.log(age);
+
+// // var monPrenom = prompt();
+// // document.write(monPrenom);
+
+// // const PAYS = "france";
+// // document.write(PAYS);
+
+// var unChiffre = "12.158";
+// console.log(unChiffre);
+// var monChiffre = parseInt(unChiffre);
+// console.log(monChiffre);
+// unChiffre = parseFloat(unChiffre);
+// console.log(unChiffre);
+
+// //number
+
+// var nbLettre = 259;
+// console.log(nbLettre);
+// console.log(typeof nbLettre);
+
+// nbLettre = nbLettre.toString();
+// console.log(nbLettre);
+// console.log(typeof nbLettre);
+
+// affichage d'une boite de dialogue avec le formulaire:
+
+var newFirstName = prompt();
+document.write(newFirstName);
+console.log(newFirstName);
+alert(newFirstName);
+
+// Différents types de fonctions :
+
+// 1 - une fonction avec au moins un paramètre sans valeur de retour
+// 2 - une fonction avec au moins un paramètre avec une valeur de retour,
+// 3 - une fonction sans paramètre sans valeur de retour ,
+// 4 - une fonction sans paramètre avec une valeur de retour .
