9e5c2d2815d4d4d03d4672bbb142b940c86a554c
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-06-20 16:28:55 +0200

    programme tableau aléatoire

---
+++ exercice/js/tableau_aleatoire.js
@@ -0,0 +1,20 @@
+var developpeurs = [
+  "Alexis",
+  "Alin",
+  "Cynthia",
+  "Faïssal",
+  "Jérémy",
+  "Karima",
+  "Michel",
+  "Narcis",
+  "Nawal",
+  "Oliver",
+  "Rahim",
+  "Wassila",
+];
+
+var developpeurs_affiche = Math.floor(Math.random() * developpeurs.length);
+
+var affiche = developpeurs[developpeurs_affiche];
+
+document.write(affiche);
