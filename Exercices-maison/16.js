e09610225f95453d084f8ee68eba8bb0de10a727
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-06-22 11:44:21 +0200

    js 16

--- Exercices-maison/16.js
+++ Exercices-maison/16.js
@@ -1,12 +1,27 @@
 // alert('test');
 
-var disMoi = "";
-var i = 1;
+// var disMoi = "";
+// var i = 1;
 
-while ((disMoi !== "oui") && (disMoi !== "non") && (disMoi !== "Oui") && (disMoi !== "Non") && (disMoi !== "OUI") && (disMoi !== "NON")) {
-    disMoi = prompt("Allez!! On joue à ni oui ni non?");
-    document.write('"' + disMoi + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
-    i++;
+// while ((disMoi !== "oui") || (disMoi !== "non") || (disMoi !== "Oui") || (disMoi !== "Non") || (disMoi !== "OUI") || (disMoi !== "NON")) {
+//     disMoi = prompt("Allez!! On joue à ni oui ni non?");
+//     document.write('"' + disMoi + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
+//     i++;
+// }
+// i--;//parce qu'il y a une incrémentation en trop
+// document.write("<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " + i + " tours.</p>");
+
+var ouiNon;
+var j = 0;
+
+while (ouiNon !== "oui" && ouiNon !== "non") {
+  ouiNon = prompt("Allez ! jouons à ni oui ni non ! ").toLowerCase();
+  console.log(
+    `${ouiNon} n'est pas une réponse acceptable , recommence avec oui ou non ? :D`
+  );
+  j++;
 }
-i--;//parce qu'il y a une incrémentation en trop
-document.write("<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " + i + " tours.</p>");
+
+document.write(
+  `<p style='background-color: black; text-align:center; color:white;font-size:60px; margin:0;'>tu as perdu Looser ! tu as tenu ${j} tour</p>`
+);
--- Exercices-maison/17.js
+++ Exercices-maison/17.js
@@ -2,16 +2,17 @@ var dieze = "#";
 
 // Avec For
 
-// for (i = 1; i <= 7; i++) {
-//   if (dieze.length === i) {
-//     document.write(`<p>${dieze}</p>`);
-//     dieze += "#";
-//   }
-// }
+for (i = 1; i <= 7; i++) {
+  if (dieze.length === i) {
+    document.write(`<p>${dieze}</p>`);
+    dieze += "#";
+  }
+}
 
 // Avec While
 
-while (dieze.length <= 7) {
-  document.write(`<p>${dieze}</p>`);
-  dieze += "#";
+var diezeWhile = "#"
+while (diezeWhile.length <= 7) {
+  document.write(`<p>${diezeWhile}</p>`);
+  diezeWhile += "#";
 }
