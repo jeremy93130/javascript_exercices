b218c4435186550af0ff6f77dda425fd63741182
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-06-23 10:58:02 +0200

    exercice 18

--- Exercices-maison/18.js
+++ Exercices-maison/18.js
@@ -1,7 +1,11 @@
-// Solution utilisant une série de if/else
-for (var nombre = 1; nombre <= 100; nombre++) {
-    if ((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
-    else if (nombre % 3 === 0) console.log("Fizz");
-    else if (nombre % 5 === 0) console.log("Buzz");
-    else console.log(nombre);
+for (i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    document.write(`<p>${i} = FizzBuzz</p>`);
+  } else if (i % 5 === 0) {
+    document.write(`<p>${i} = Buzz</p>`);
+  } else if (i % 3 === 0) {
+    document.write(`<p>${i} = Fizz</p>`);
+  } else {
+    document.write(`<p>${i}</p>`);
+  }
 }
